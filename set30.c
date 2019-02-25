de7deaf3fa8c2f3888cc1c9f84a14e94732274ee
Boopathivsr <47106149+Boopathivsr@users.noreply.github.com>
2019-02-25 16:19:20 +0530

    Update set30.c

--- set30.c
+++ set30.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int n,i=1,a;
-	scanf("%d",&n);
-	for(i=1;i<=5;i++)
-	{
-		a=n*i;
-		printf("%d ",a);
-	}
+int a,i=1,ans;
+printf("multiplication of number:?");
+scanf("%d",&a);
+while(i<=5)
+{
+ans=i*a;
+printf("%d\n",ans);
+i++;
+}
 }
