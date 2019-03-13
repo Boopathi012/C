b150990a189e5e0ceb39a3d1ed8dee481d12c662
Boopathivsr <47106149+Boopathivsr@users.noreply.github.com>
2019-03-13 20:08:07 +0530

    Create set53.c

---
+++ set53.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+   char s1,s2,s3;
+   printf("String 1");
+   scanf("%s",s1);
+   printf("String 2");
+   scanf("%s",s2);
+   s3=s1+s2;
+   printf("%s",s3);
+}
